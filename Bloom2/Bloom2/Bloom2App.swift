9a333b0c331e3f3707267158997f7da39e0d88cf
Dana AlGhadeer <danaabdullahalghadeer@gmail.com>
2025-09-29 17:06:51 +0300

    Add files via upload

---
+++ Bloom2/Bloom2/Bloom2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bloom2App.swift
+//  Bloom2
+//
+//  Created by Dana AlGhadeer on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Bloom2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bloom2/Bloom2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bloom2
+//
+//  Created by Dana AlGhadeer on 29/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
